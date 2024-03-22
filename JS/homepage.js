3c3f2e1ec7bbdac4c36fb416831fef6b33425292
abhayjadav96 <93526613+abhayjadav96@users.noreply.github.com>
2024-03-22 02:04:09 -0400

    Update

--- JS/homepage.js
+++ JS/homepage.js
@@ -16,29 +16,75 @@ let movies = [];
 
 function homepageDisplay(arr){
 
-    document.querySelector("#movie-list").innerHTML="";
+    document.getElementById("movie-list").innerHTML="";
 
     arr.forEach((movie, index) => {
         
         let movieFrameDiv=document.createElement("div");
-        movieFrameDiv.className="movie_frame_holder";
+        movieFrameDiv.className="test_container";
 
-        let moviePoster=document.createElement("div");
-        moviePoster.className="movie_poster_holder"
+          let movie_card_holder=document.createElement("div");
+          movie_card_holder.className="test_card";
+          movieFrameDiv.appendChild(movie_card_holder); 
 
-        let movieurl=document.createElement("img");
-        movieurl.src=movie.posterurl;
-        moviePoster.appendChild(movieurl);
-        movieurl.style.height= "45vh";
-        movieurl.style.width="100%";
-        movieurl.style.objectFit="cover";
+            let movieImageHolder=document.createElement("div");
+            movieImageHolder.className="img_holder";
+            movie_card_holder.appendChild(movieImageHolder); 
 
-        let movieTitle= document.createElement("div");
-        movieTitle.className= "movie_title_holder";
-        movieTitle.append(movie.title);
+              let movieImage=document.createElement("img");
+              movieImage.src=movie.posterurl;
+              movieImage.alt="movie-poster";
+              movieImageHolder.appendChild(movieImage);
 
-        movieFrameDiv.appendChild(moviePoster);
-        movieFrameDiv.appendChild(movieTitle);
+            let movieContentHolder=document.createElement("div");
+            movieContentHolder.className="content_holder";
+            movie_card_holder.appendChild(movieContentHolder); 
+
+              let movieTitleContainer=document.createElement("div");
+              movieTitleContainer.className="movie_title";
+              movieContentHolder.appendChild(movieTitleContainer);
+
+                let movieTitle= document.createElement("h3");
+                movieTitle.className= "title";
+                movieTitle.innerText="Title:"+ movie.title;
+                movieTitleContainer.appendChild(movieTitle);
+
+                let genresDetails= document.createElement("div");
+                genresDetails.className= "genres";
+                genresDetails.innerText="Genres:" +movie.genres;
+                movieTitleContainer.appendChild(genresDetails);
+
+              
+              let movieStoryContainer=document.createElement("div");
+              movieStoryContainer.className="movie_storyline";
+              movieStoryContainer.innerText=movie.storyline;
+              movieContentHolder.appendChild(movieStoryContainer);
+
+
+              let movieActionContainer=document.createElement("div");
+              movieActionContainer.className="movie_actions";
+              movieContentHolder.append(movieActionContainer);
+          
+                let ratingContainer=document.createElement("div");
+                ratingContainer.className="rating";
+                movieActionContainer.append(ratingContainer);
+
+                let ratingText=document.createElement("p");
+                ratingText.innerText="Ratings: ";
+                ratingContainer.appendChild(ratingText);
+
+                let ratingIcon=document.createElement("i");
+                ratingIcon.className="fa-regular";
+                ratingIcon.className="far fa-star";
+                ratingContainer.appendChild(ratingIcon);
+
+                let durationContainer=document.createElement("div");
+                durationContainer.className="duration";
+                movieActionContainer.append(durationContainer);
+
+                let durationText=document.createElement("p");
+                durationText.innerHTML="Duration: "+movie.duration+" min" ;
+                durationContainer.appendChild(durationText);
 
         document.querySelector("#movie-list").appendChild(movieFrameDiv); 
         
